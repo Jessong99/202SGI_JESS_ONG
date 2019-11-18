f324dfd98ea696f915e56c7abd5eaa4c3c42defb
Jessong99 <41329895+Jessong99@users.noreply.github.com>
2019-11-19 00:37:41 +0800

    Initial commit

---
+++ app/src/main/java/com/example/a202sgi_jess_ong/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.a202sgi_jess_ong;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+    }
+}
