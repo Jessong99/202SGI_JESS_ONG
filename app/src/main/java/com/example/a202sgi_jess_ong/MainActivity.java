5a7f69713388586fd9d76bdad33321992133f44a
Jessong99 <41329895+Jessong99@users.noreply.github.com>
2019-11-19 15:42:44 +0800

    342

--- app/src/main/java/com/example/a202sgi_jess_ong/MainActivity.java
+++ app/src/main/java/com/example/a202sgi_jess_ong/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.a202sgi_jess_ong;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -7,25 +8,45 @@ import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     DrawerLayout mDrawerLayout;
     ActionBarDrawerToggle mDrawerToggle;
-    Toolbar toolbar;
+    Toolbar mToolbar;
+    NavigationView mNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-        toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        mToolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
 
+        //drawer menu settings
         mDrawerLayout = findViewById(R.id.drawer);
-        mDrawerToggle = new ActionBarDrawerToggle(this,mDrawerLayout,toolbar,R.string.open_drawer,R.string.close_drawer);
+        mDrawerToggle = new ActionBarDrawerToggle(this,mDrawerLayout,mToolbar,R.string.open_drawer,R.string.close_drawer);
         mDrawerLayout.addDrawerListener(mDrawerToggle);
         mDrawerToggle.setDrawerIndicatorEnabled(true);
         mDrawerToggle.syncState();
+
+        mNavigationView = findViewById(R.id.navigationView);
+        mNavigationView.setNavigationItemSelectedListener(this);
+    }
+
+    //menu option
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()){
+            case R.id.home:
+                Toast.makeText(this,"Home",Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return false;
     }
 }
