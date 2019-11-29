1cb1617ccc49627338726711ca33af162577606d
Jessong99 <41329895+Jessong99@users.noreply.github.com>
2019-11-30 03:47:34 +0800

    Update NoteUtils.java

--- app/src/main/java/com/example/a202sgi_jess_ong/NoteUtils.java
+++ app/src/main/java/com/example/a202sgi_jess_ong/NoteUtils.java
@@ -13,24 +13,34 @@ public class NoteUtils {
     private static final int DAY_MILLIS = 24*HOUR_MILLIS;
     private static final int WEEK_MILLIS = 7*DAY_MILLIS;
 
+    private static DateFormat day = new SimpleDateFormat("dd", Locale.US);
+    private static DateFormat month = new SimpleDateFormat("MMM", Locale.US);
+    private static DateFormat year = new SimpleDateFormat("dd", Locale.US);
+
+    private static DateFormat week = new SimpleDateFormat("EEEE", Locale.US);
+    private static DateFormat date = new SimpleDateFormat("dd/MMM/yyyy", Locale.US);
+    private static DateFormat today = new SimpleDateFormat("hh:mm aaa", Locale.US);
+
     public static String dateFromLong(long time){
         if (time<100000000000L){
             time *= 1000;
         }
         long now = System.currentTimeMillis();
-        DateFormat week = new SimpleDateFormat("EEEEE", Locale.US);
-        DateFormat date = new SimpleDateFormat("dd/MMM/yyyy", Locale.US);
-        DateFormat today = new SimpleDateFormat("hh:mm aaa", Locale.US);
+
+        int currentD = Integer.valueOf(day.format(new Date(now)));
+        int noteD = Integer.valueOf(day.format(new Date(time)));
+        String currentM = month.format(new Date(now));
+        String noteM = month.format(new Date(time));
+        int currentY = Integer.valueOf(year.format(new Date(now)));
+        int noteY = Integer.valueOf(year.format(new Date(time)));
 
         final long diff = now - time;
 
         if (diff < MINUTE_MILLIS){
             return "Just now";
-        }else if (diff >= MINUTE_MILLIS && diff < DAY_MILLIS){
+        }else if (diff >= MINUTE_MILLIS && currentD == noteD && currentM == noteM && currentY == noteY){
             return today.format(new Date(time));
-        }else if (diff >= DAY_MILLIS && diff < 2 * DAY_MILLIS){
-            return "Yesterday";
-        }else if (diff >= 2 * DAY_MILLIS && diff < WEEK_MILLIS){
+        }else if (diff >= DAY_MILLIS && diff < WEEK_MILLIS){
             return week.format(new Date(time));
         }else{
             return date.format(new Date(time));
