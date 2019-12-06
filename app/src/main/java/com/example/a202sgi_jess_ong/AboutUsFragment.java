9c5aab582b102f46a1922c2dcaf58e171251863b
Jessong99 <41329895+Jessong99@users.noreply.github.com>
2019-12-06 16:49:44 +0800

    4:49

--- app/src/main/java/com/example/a202sgi_jess_ong/AboutUsFragment.java
+++ app/src/main/java/com/example/a202sgi_jess_ong/AboutUsFragment.java
@@ -19,15 +19,6 @@ public class AboutUsFragment extends Fragment{
         @Override
         public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
                 View view =  inflater.inflate(R.layout.fragment_about_us,container,false);
-
-                btnShare = (Button)view.findViewById(R.id.shareApp);
-                btnShare.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View view) {
-                                //todo share app
-                        }
-                });
-
                 return view;
         }
 }
